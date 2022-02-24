954ff57c611063f72d699afb0720ff6f7b37c044
Yuanuox <yuanuox@outlook.com>
2022-02-25 01:41:08 +0800

    发布版本22.02.25

--- src/main/java/org/appxi/smartlib/App.java
+++ src/main/java/org/appxi/smartlib/App.java
@@ -26,7 +26,7 @@ import java.util.Optional;
 public class App extends WorkbenchApp {
     public static final String ID = "smartLibrary";
     public static final String NAME = "智悲研藏";
-    public static final String VERSION = "22.02.23";
+    public static final String VERSION = "22.02.25";
     private static App instance;
 
     public App() {
