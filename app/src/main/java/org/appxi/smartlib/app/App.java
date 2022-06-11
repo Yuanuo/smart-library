ec33c166328efcccb84c4997c091822c36e8c5de
Yuanuox <yuanuox@outlook.com>
2022-06-11 10:08:06 +0800

    更新版本号

--- app/src/main/java/org/appxi/smartlib/app/App.java
+++ app/src/main/java/org/appxi/smartlib/app/App.java
@@ -30,7 +30,7 @@ import java.util.Optional;
 public class App extends WorkbenchApp {
     public static final String ID = "smartLibrary";
     public static final String NAME = "智悲研藏";
-    public static final String VERSION = "22.06.01";
+    public static final String VERSION = "22.06.11";
     private static App instance;
 
     public App() {
